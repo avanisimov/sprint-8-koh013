056760a3ae52804818a311f223d9bfb45ccc6101
Aleksei Anisimov <suxelshock@gmail.com>
2023-12-26 10:17:16 +0300

    Полезные интенты

--- app/src/main/java/ru/yandex/practicum/sprint8koh13/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh13/MainActivity.kt
@@ -1,59 +1,121 @@
 package ru.yandex.practicum.sprint8koh13
 
+import android.content.ActivityNotFoundException
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.LabeledIntent
 import android.content.res.Configuration
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.button.MaterialButton
+import ru.yandex.practicum.sprint8koh13.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var lastOnStop: Long = 0
+
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.appSettings.setOnClickListener {
+            startActivity(
+                Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", packageName, null)
+                }
+            )
+        }
+        binding.relaunch.setOnClickListener {
+            finish()
+            startActivity(
+                packageManager.getLaunchIntentForPackage(packageName)
+            )
+        }
+        binding.chooser.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_VIEW
+            }
+            startActivity(Intent.createChooser(sendIntent, "Объяснение зачем это нужно"))
+        }
+        binding.notification.setOnClickListener {
+            val intent = Intent()
+            intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
 
-        val btn1 = findViewById<Button>(R.id.btn_1)
-        val btn2 = findViewById<MaterialButton>(R.id.btn_2)
+            //for Android 5-7
+            //intent.putExtra("app_package", packageName)
+            //intent.putExtra("app_uid", applicationInfo.uid)
 
-        this.resources
-        Log.d("SPRINT_8", "$this onCreate")
-    }
+            // for Android 8 and above
+            intent.putExtra("android.provider.extra.APP_PACKAGE", packageName)
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("SPRINT_8", "$this onStart")
-//        startUpdate()
-
-        if (System.currentTimeMillis() - lastOnStop > 15000) {
-
+            startActivity(intent)
         }
 
-    }
+        binding.skype.setOnClickListener {
+            val skypeIntent = packageManager.getLaunchIntentForPackage("com.skype.raider")
+            if (skypeIntent != null) {
+                startActivity(skypeIntent)
+            } else {
+                try {
+                    startActivity(Intent(Intent.ACTION_SEND))
+                } catch (e: ActivityNotFoundException) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "ActivityNotFoundException",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+        binding.filter.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+            }
+            val resInfoList = packageManager.queryIntentActivities(shareIntent, 0)
+            val extraIntents = resInfoList
+                .filter {
+                    it.activityInfo.packageName.startsWith("com.google.android.apps")
+                }
+                .map { resolveInfo ->
+                    val intent = Intent()
+                    intent.component = ComponentName(
+                        resolveInfo.activityInfo.packageName,
+                        resolveInfo.activityInfo.name
+                    )
+                    intent.action = Intent.ACTION_SEND
+                    intent.type = "text/plain"
+                    intent.`package` = resolveInfo.activityInfo.packageName
+                    LabeledIntent(
+                        intent,
+                        resolveInfo.activityInfo.packageName,
+                        resolveInfo.labelRes,
+                        resolveInfo.icon
+                    )
+                }.toMutableList()
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("SPRINT_8", "$this onResume")
-    }
+            val chooser = Intent.createChooser(extraIntents.removeAt(0), "My share")
+            chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents.toTypedArray())
+            startActivity(chooser)
+        }
 
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("SPRINT_8", "$this onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("SPRINT_8", "$this onStop")
-//        stopUpdate()
-        lastOnStop = System.currentTimeMillis()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("SPRINT_8", "$this onDestroy")
+        binding.email.setOnClickListener {
+            val email = "example@gmail.com"
+            val text = "emailtext"
+            val subject = "emailsubject"
+            startActivity(
+                Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:$email?text=$text&subject=$subject")
+                }
+            )
+        }
     }
 }
