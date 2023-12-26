1230e2b397dc9d2769892b9583fd299a28cebf7a
Aleksei Anisimov <suxelshock@gmail.com>
2023-12-26 10:07:35 +0300

    Жизненный цикл activity

--- app/src/main/java/ru/yandex/practicum/sprint8koh13/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh13/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.sprint8koh13
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,9 @@ import android.widget.Button
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+    private var lastOnStop: Long = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,7 +19,41 @@ class MainActivity : AppCompatActivity() {
         val btn1 = findViewById<Button>(R.id.btn_1)
         val btn2 = findViewById<MaterialButton>(R.id.btn_2)
 
-        Log.d("SPRINT_*", "btn1 $btn1")
-        Log.d("SPRINT_*", "btn2 $btn2")
+        this.resources
+        Log.d("SPRINT_8", "$this onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("SPRINT_8", "$this onStart")
+//        startUpdate()
+
+        if (System.currentTimeMillis() - lastOnStop > 15000) {
+
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SPRINT_8", "$this onResume")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SPRINT_8", "$this onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SPRINT_8", "$this onStop")
+//        stopUpdate()
+        lastOnStop = System.currentTimeMillis()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SPRINT_8", "$this onDestroy")
     }
 }
